6c1a7041cd2cb0b5fb1124bcfe4fe8d9e10abc57
kuzn3tsov <lem.treursic@gmail.com>
2025-11-13 14:17:27 +0100

    Fix vite config and update for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/scum-raid-calc/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/scum-raid-calculator-vue/' : '/',
 })
