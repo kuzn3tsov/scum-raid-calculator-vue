064c38e55a1132386c485f1303486022e9d63543
kuzn3tsov <lem.treursic@gmail.com>
2025-11-13 14:12:55 +0100

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV === 'production' ? '/scum-raid-calc/' : '/',
 })
